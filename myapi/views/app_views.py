e6f612579e89d226ecdf4c04b5943b89bb9946b7
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2024-05-07 15:10:51 +0700

    fix invalid person_id bug

--- myapi/views/app_views.py
+++ myapi/views/app_views.py
@@ -1,5 +1,5 @@
 from myapi.chart_utils import *
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import View, TemplateView
 
 
@@ -34,35 +34,27 @@ class HeightView(BaseVisualizeView):
 class WeightView(BaseVisualizeView):
     strategy_class = WeightStrategy
 
-# class SleepView(TemplateView):
-#     template_name = 'myapi/sleep_visualize.html'
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-
-#         if 'person_id' in context:
-#             sleep_time_chart = SleepTimeStrategy.get_chart_by_id(context['person_id'])
-#             context['sleep_time_chart'] = sleep_time_chart or "Sleep Time Chart is unavailable"
-
-#             sleep_duration_chart = SleepDurationStrategy.get_chart_by_id(context['person_id'])
-#             context['sleep_duration_chart'] = sleep_duration_chart or "Sleep Duration Chart is unavailable"
-
-#             sleep_score_chart = SleepScoreStrategy.get_chart_by_id(context['person_id'])
-#             context['sleep_score_chart'] = sleep_score_chart or "Sleep Score Chart is unavailable"
-#         return context
-
-# TODO Fix bugs
 class SleepView(View):
     template_name = 'myapi/sleep_visualize.html'
 
     def get(self, request, *args, **kwargs):
-        if "person_id" not in self.kwargs:
+        person_id = self.kwargs.get("person_id")
+
+        if not person_id:
             return render(request, self.template_name)
-        
-        person_id = self.kwargs["person_id"]
-        sleep_time_chart = SleepTimeStrategy.get_chart_by_id(person_id) or "Sleep Time Chart is unavailable"
-        sleep_duration_chart = SleepDurationStrategy.get_chart_by_id(person_id) or "Sleep Duration Chart is unavailable"
-        sleep_score_chart = SleepScoreStrategy.get_chart_by_id(person_id) or "Sleep Score Chart is unavailable"
+
+        every_person_id = Person.objects.values_list("person_id", flat=True)
+        person_has_sleep = Sleep.objects.filter(person_id=person_id).exists()
+
+        if (person_id in every_person_id) and person_has_sleep:
+            sleep_time_chart = SleepTimeStrategy.get_chart_by_id(person_id)
+            sleep_duration_chart = SleepDurationStrategy.get_chart_by_id(person_id)
+            sleep_score_chart = SleepScoreStrategy.get_chart_by_id(person_id)
+        else:
+            sleep_time_chart = "Sleep Time Chart is unavailable"
+            sleep_duration_chart = "Sleep Duration Chart is unavailable"
+            sleep_score_chart = "Sleep Score Chart is unavailable"
 
         context = {
             'person_id': person_id,
@@ -74,7 +66,4 @@ class SleepView(View):
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        person_id = request.POST.get("person_id")
-        # print(person_id)
-
-        return redirect('sleep-detail', person_id=person_id)
+        return redirect('sleep-detail', person_id=request.POST.get("person_id"))
